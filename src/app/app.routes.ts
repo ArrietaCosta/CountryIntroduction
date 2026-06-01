d37e47511d47f13285debc206a471df99803f5ae
ArrietaCosta <natashabmw888@gmail.com>
2026-06-01 22:59:01 +0800

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+
+export const routes: Routes = [
+  { path: 'privacy', component: PrivacyPolicyComponent }
+];
